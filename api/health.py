7d36b4b302141480e3347a6ffa94d07a51471ec8
Tilahun Alene <tilahunalenee@gmail.com>
2026-07-14 23:21:33 +0300

    Remove broken Vercel backend serverless config

--- api/health.py
+++
@@ -1,9 +0,0 @@
-from http import HTTPStatus
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse
-
-app = FastAPI()
-
-@app.get('/api/health')
-def health():
-    return JSONResponse({'status': 'ok'}, status_code=HTTPStatus.OK)
